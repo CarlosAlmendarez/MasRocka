b463e47edf8b35a8f1dd08dc25e5e7a47e53121a
Margarita Mendez <maggielee97@icloud.com>
2019-12-01 13:08:49 -0600

    Product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $name = $request->input('nombre');
+        $existencias = $request->input('existencias');
+        $path = $request->input('path');
+
+        $producto = new Product();
+        $producto->nombre = $name;
+        $producto->existencias = $existencias;
+        $producto->path = $path;
+        $producto->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $productId)
+    {
+        $producto = Product::FindOrFail($productId);
+
+        $name = $request->input('nombre');
+        $existencias = $request->input('existencias');
+        $path = $request->input('path');
+
+        $producto->nombre = $name;
+        $producto->existencias = $existencias;
+        $producto->path = $path;
+
+        $producto->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
